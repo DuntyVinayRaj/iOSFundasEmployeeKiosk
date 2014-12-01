3fbf5b035511b2967409300a82997b5c149e0f5d
Vinay Raj <vinayraj.dunty@myntra.com>
2014-12-01 18:21:42 +0530

    Initial Commit

---
+++ EmployeeKiosk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EmployeeKiosk
+//
+//  Created by Vinay Raj Dunty on 01/12/14.
+//  Copyright (c) 2014 MyTectra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
